83868524a854446055ae4392cbd16fa612970a47
TheWhisp <daavis.90@gmail.com>
2013-08-24 19:43:40 +0300

    Revert "remove 'detect' from power code"
    This reverts commit e4351622e3271e18fe18b51a790078146bdc5dd4.

--- drivers/net/wireless/ath/ath6kl/sdio.c
+++ drivers/net/wireless/ath/ath6kl/sdio.c
@@ -29,7 +29,7 @@
 #include "debug.h"
 #include "cfg80211.h"
 
-extern void wlan_setup_power(int on);
+extern void wlan_setup_power(int on, int detect);
 
 struct ath6kl_sdio {
 	struct sdio_func *func;
@@ -1424,7 +1424,7 @@ static int __init ath6kl_sdio_init(void)
 {
 	int ret;
 
-	wlan_setup_power(1);
+	wlan_setup_power(1,1);
 	ret = sdio_register_driver(&ath6kl_sdio_driver);
 	if (ret)
 		ath6kl_err("sdio driver registration failed: %d\n", ret);
@@ -1435,7 +1435,7 @@ static int __init ath6kl_sdio_init(void)
 static void __exit ath6kl_sdio_exit(void)
 {
 	sdio_unregister_driver(&ath6kl_sdio_driver);
-	wlan_setup_power(0);
+	wlan_setup_power(0,1);
 }
 
 module_init(ath6kl_sdio_init);
